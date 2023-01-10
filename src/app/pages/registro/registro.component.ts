5d1c165ee12713b8367b0a238f373686539b06bd
elizabethnag1311 <109186428+elizabethnag1311@users.noreply.github.com>
2023-01-10 18:24:29 -0300

    Formularios

--- src/app/pages/registro/registro.component.ts
+++ src/app/pages/registro/registro.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { UsuarioModel } from 'src/app/models/usuario.model';
 
 @Component({
   selector: 'app-registro',
-  templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.css']
+  templateUrl: './registro.component.html'
 })
-export class RegistroComponent {
+export class RegistroComponent implements OnInit {
+
+  usuario: UsuarioModel = new UsuarioModel;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onSubmit( form: NgForm){
+
+    if ( form.invalid ) { return; }
+
+    console.log('formulaio enviado');
+    console.log(this.usuario);
+    console.log(form);
+  }
 
 }
