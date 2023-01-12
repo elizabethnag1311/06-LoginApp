43453103d5eb94849f37082991103e6d9b7858c5
elizabethnag1311 <109186428+elizabethnag1311@users.noreply.github.com>
2023-01-11 21:11:47 -0300

    Manejo de Errores

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../../services/auth.service';
 import { UsuarioModel } from '../../models/usuario.model';
+
+import Swal from 'sweetalert2';
+
 
 
 @Component({
@@ -10,18 +16,38 @@ import { UsuarioModel } from '../../models/usuario.model';
 })
 export class LoginComponent implements OnInit {
 
-  loginPage: UsuarioModel= new UsuarioModel 
+  usuario: UsuarioModel= new UsuarioModel 
 
-  constructor() {}
+  constructor( private auth: AuthService,
+               private router: Router) {}
 
   ngOnInit() {}
 
   login( form: NgForm ) {
 
-    if (form.invalid) { return; }
-    
-    console.log('login enviado');
-    console.log(this.loginPage);
-    console.log(form);
+    if ( form.invalid ) { return; }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+
+    this.auth.login( this.usuario )
+    .subscribe( resp => {
+
+      console.log(resp);
+      Swal.close();
+      this.router.navigateByUrl('/home')
+
+    }, (err) => {
+      console.log(err.error.error.message);
+      Swal.fire({
+        icon: 'error',
+        title: 'Error al autenticar',
+        text: err.error.error.message
+      });
+    });
   }
 }
